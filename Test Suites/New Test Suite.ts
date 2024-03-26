febe77e128f6fbaed4ca1bae189ebe07fa6146a7
Kim Hang <minioncrush115@gmail.com>
2024-03-26 11:19:53 +0700

    update test3

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ad81c617-6e6a-4553-b83c-0416ea38686c</testSuiteGuid>
+   <testCaseLink>
+      <guid>2cb86e0a-2f35-43e7-b819-1e3b92518ddf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test3</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>cf55f7eb-53b5-4902-8115-ecefe58ad84d</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/data3</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>cf55f7eb-53b5-4902-8115-ecefe58ad84d</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>TK</value>
+         <variableId>d6bd078a-c6b3-4a9c-8ddf-91c171597769</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>cf55f7eb-53b5-4902-8115-ecefe58ad84d</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>MK</value>
+         <variableId>28048b37-a636-4912-a924-0379d67c411e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
